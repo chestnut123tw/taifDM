9148b1ea33360d05c15a363d3e10c5f4fcbf349c
chestnut123tw <chestnut123tw@gmail.com>
2025-07-23 13:03:37 +0800

    Add taxon filter and more chart options

--- app.R
+++ app.R
@@ -30,9 +30,21 @@ data_version <- stringr::str_extract(basename(csv_path), "\\d{8}")
 name_plantae_taicol <- read_csv(csv_path)
 
 name_plantae_taicol <- name_plantae_taicol %>%
-  mutate(taxon_id = paste0("<a href='", "https://taicol.tw/taxon/", taxon_id,"' target='_blank'>", taxon_id,"</a>")) %>%
+  mutate(
+    taxon_id = paste0(
+      "<a href='",
+      "https://taicol.tw/taxon/",
+      taxon_id,
+      "' target='_blank'>",
+      taxon_id,
+      "</a>"
+    )
+  ) %>%
   select(-name_id) %>%
   relocate(ends_with("name_c"), .after = taxon_id)
+
+# boolean columns for checkbox filters
+is_cols <- grep("^is_", names(name_plantae_taicol), value = TRUE)
 
 
 ui <- fluidPage(
@@ -40,10 +52,17 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       em(paste0("(version ", data_version, ")")),
-      checkboxInput(
-        inputId = "isEndemic",
-        label = "Endemic",
-        value = FALSE
+      selectInput(
+        inputId = "tax_rank",
+        label = "Taxonomic Rank",
+        choices = c("All" = "", sort(unique(name_plantae_taicol$rank))),
+        selected = ""
+      ),
+      ## checkbox filters for all `is_` columns
+      tagList(
+        lapply(is_cols, function(x) {
+          checkboxInput(inputId = x, label = x, value = FALSE)
+        })
       ),
       selectInput(
         inputId = "filter_column",
@@ -74,6 +93,12 @@ ui <- fluidPage(
         selected = ""
       ),
       selectInput(
+        inputId = "fill_col",
+        label = "Fill",
+        choices = c("None" = "", names(name_plantae_taicol)),
+        selected = ""
+      ),
+      selectInput(
         inputId = "chart_type",
         label = "Chart Type",
         choices = c(
@@ -86,7 +111,11 @@ ui <- fluidPage(
           "Violin" = "violin",
           "Area" = "area",
           "Waffle" = "waffle",
-          "Calendar" = "calendar"
+          "Calendar" = "calendar",
+          "Stacked Bar" = "stackbar",
+          "Pie" = "pie",
+          "Heatmap" = "heatmap",
+          "Lollipop" = "lollipop"
         ),
         selected = "bar"
       )
@@ -104,18 +133,32 @@ server <- function(input, output) {
 
   filtered_data <- reactive({
     df <- name_plantae_taicol
-    if (input$isEndemic) {
-      df <- df %>% filter(is_endemic)
+
+    # taxonomic rank filter
+    if (nzchar(input$tax_rank)) {
+      df <- df %>% filter(rank == input$tax_rank)
     }
+
+    # boolean checkbox filters
+    for (col in is_cols) {
+      val <- input[[col]]
+      if (isTRUE(val)) {
+        df <- df %>% filter(.data[[col]])
+      }
+    }
+
     if (nzchar(input$filter_value) && input$filter_column %in% names(df)) {
-      df <- df %>% filter(str_detect(as.character(.data[[input$filter_column]]),
-                                    fixed(input$filter_value, ignore_case = TRUE)))
+      df <- df %>% filter(
+        str_detect(as.character(.data[[input$filter_column]]),
+                   fixed(input$filter_value, ignore_case = TRUE))
+      )
     }
+
     df
   })
 
   output$dataTable <- renderDT({
-    datatable(
+  datatable(
       filtered_data(),
       filter = list(
         position = "top"
@@ -137,10 +180,11 @@ server <- function(input, output) {
     x <- rlang::sym(input$x_col)
     y <- if (nzchar(input$y_col)) rlang::sym(input$y_col) else NULL
     color <- if (nzchar(input$color_col)) rlang::sym(input$color_col) else NULL
+    fill <- if (nzchar(input$fill_col)) rlang::sym(input$fill_col) else NULL
 
-    p <- ggplot(df)
+    chart <- input$chart_type
 
-    if (input$chart_type == "waffle") {
+    if (chart == "waffle") {
       counts <- df %>% count(!!x)
       n_cols <- 10
       waffle_df <- counts %>%
@@ -153,7 +197,7 @@ server <- function(input, output) {
         scale_y_continuous(NULL, breaks = NULL) +
         scale_x_continuous(NULL, breaks = NULL) +
         guides(fill = guide_legend(title = rlang::as_label(x)))
-    } else if (input$chart_type == "calendar") {
+    } else if (chart == "calendar") {
       df <- df %>% mutate(date = as.Date(as.character(!!x)))
       df <- df %>% count(date) %>%
         mutate(week = lubridate::isoweek(date),
@@ -161,52 +205,55 @@ server <- function(input, output) {
       p <- ggplot(df, aes(week, wday, fill = n, text = date)) +
         geom_tile(color = "white") +
         scale_y_reverse(breaks = 1:7,
-                        labels = c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")) +
+                        labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")) +
         labs(x = "Week", y = NULL, fill = "Count")
+    } else if (chart == "heatmap") {
+      req(!is.null(y))
+      df_sum <- df %>% count(!!x, !!y)
+      p <- ggplot(df_sum, aes(!!x, !!y, fill = n)) + geom_tile()
+    } else if (chart == "stackbar") {
+      req(!is.null(y))
+      df_sum <- df %>% count(!!x, !!y)
+      p <- ggplot(df_sum, aes(!!x, n, fill = !!y)) + geom_col(position = "stack")
+    } else if (chart == "pie") {
+      counts <- df %>% count(!!x)
+      p <- ggplot(counts, aes(x = "", y = n, fill = !!x)) +
+        geom_col(width = 1) +
+        coord_polar("y") +
+        labs(x = NULL, y = NULL)
+    } else if (chart == "lollipop") {
+      req(!is.null(y))
+      p <- ggplot(df, aes(!!x, !!y)) +
+        geom_segment(aes(xend = !!x, yend = 0), colour = "grey") +
+        geom_point(aes(color = !!color, fill = !!fill), size = 3)
     } else {
-      if (input$chart_type == "bar") {
-        if (is.null(y)) {
-          p <- ggplot(df, aes(x = !!x))
-          if (!is.null(color)) p <- p + aes(fill = !!color)
-          p <- p + geom_bar()
-        } else {
-          group_vars <- c(as.character(x), as.character(y))
-          if (!is.null(color)) group_vars <- c(group_vars, as.character(color))
-          df_sum <- df %>% count(!!!syms(group_vars))
-          aes_args <- list(x = !!x, y = n)
-          if (!is.null(color)) aes_args$fill <- !!color
-          p <- ggplot(df_sum) + do.call(aes, aes_args) + geom_col(position = "dodge")
-        }
-      } else {
-        aes_args <- list(x = !!x)
-        if (!is.null(y) && input$chart_type %in% c("line", "scatter", "boxplot",
-                                                   "violin", "area")) {
-          aes_args$y <- !!y
-        }
-        if (!is.null(color)) aes_args$colour <- !!color
-        p <- ggplot(df) + do.call(aes, aes_args)
+      aes_args <- list(x = !!x)
+      if (!is.null(y)) aes_args$y <- !!y
+      if (!is.null(color)) aes_args$colour <- !!color
+      if (!is.null(fill)) aes_args$fill <- !!fill
+      p <- ggplot(df, do.call(aes, aes_args))
 
-        if (input$chart_type == "line") {
-          p <- p + geom_line() + geom_point()
-        } else if (input$chart_type == "scatter") {
-          p <- p + geom_point()
-        } else if (input$chart_type == "boxplot") {
-          p <- p + geom_boxplot()
-        } else if (input$chart_type == "hist") {
-          p <- p + geom_histogram(bins = 30)
-        } else if (input$chart_type == "density") {
-          p <- p + geom_density()
-        } else if (input$chart_type == "violin") {
-          p <- p + geom_violin()
-        } else if (input$chart_type == "area") {
-          p <- p + geom_area()
-        }
+      if (chart == "bar") {
+        p <- p + geom_bar(stat = "count", position = "dodge")
+      } else if (chart == "line") {
+        p <- p + geom_line() + geom_point()
+      } else if (chart == "scatter") {
+        p <- p + geom_point()
+      } else if (chart == "boxplot") {
+        p <- p + geom_boxplot()
+      } else if (chart == "hist") {
+        p <- p + geom_histogram(bins = 30)
+      } else if (chart == "density") {
+        p <- p + geom_density()
+      } else if (chart == "violin") {
+        p <- p + geom_violin()
+      } else if (chart == "area") {
+        p <- p + geom_area()
       }
     }
 
     ggplotly(p)
   })
-
 }
 
 shinyApp(ui, server)
